ba7570e53f1cf59d384c7cc0c567b47b03c6b6c1
NorthStartAngle <apron.heat.transfers@proton.me>
2024-04-29 14:56:14 +0900

    Add project files.

---
+++ Controllers/MainController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace proMVC.Controllers
+{
+    public class MainController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult NavPage(int pg)
+        {
+            ViewData["page"] = pg;
+            return View("index");
+        }
+
+    
+        public string Result()
+        {
+            return "Result is 3. OK!";
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Result(int? ret)
+        {
+            return View();
+        }
+    }
+}
